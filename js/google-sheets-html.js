43fd3fcb315da19c5eefc9d43b1db3d2f03a8d2c
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-05-28 11:59:58 +0530

    pkWinnersVoice

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -143,7 +143,7 @@ $('#conDivUser1').append('<div class="colThree"><div class="imgClass1"><a href="
 
     $('#conDivTalent3').append('<div class="colThree"><div class="imgClass3"><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
       }
-   else  if(counter>=6 && counter<7){
+   else  if(counter>6 && counter<7){
 
      $('#conDivTalent4').append('<div class="colThree1"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass4"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
        }
